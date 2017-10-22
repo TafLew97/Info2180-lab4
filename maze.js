c2b067f0914e56792874d5db581cd86651417c26
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-10-21 20:43:23 -0500

    Exercise 6: Disallow cheating

--- maze.js
+++ maze.js
@@ -1,31 +1,36 @@
-//#5
+//#6
 var boundaries;
 var end;
 var start;
 var statusState;
+var mazeBoundary;
 var win = false;
 var outOfBounds = false;
 
 window.onload = function() {
+	// Selection of the HTML elements to be used 
     boundaries = document.querySelectorAll(".boundary");
     end = document.getElementById("end");
     start = document.getElementById("start");
     statusState = document.getElementById("status");
+    mazeBoundary = document.getElementById("maze");
 
+    //Assignment of functions to events to be used to run the game
     start.onclick = resetGame;
+    mazeBoundary.onmouseleave = antiCheat;
     end.onmouseover = endGame;
-    for (var i = 0; i < boundaries.length; i++) {
+    for (var i = 0; i < boundaries.length - 1; i++) { //boundaries.length - 1 was used to exclude the example boundary from being touched
 		boundaries[i].onmouseover = overBounds;
 	}
 }
 
-function overBounds(){
+function overBounds(){ // Checks if the user touches a wall 
 	if (!win){ // Used to prevent this code block from running if you have already won
 		if(!outOfBounds) // Checks if the youlose class has already been applied
 		{
 			outOfBounds = true;
 			statusState.innerText = 'You Lose! Click the "S" to reset and play again.';
-			for (var i = 0; i < boundaries.length-1; i++) {
+			for (var i = 0; i < boundaries.length - 1; i++) {
 			boundaries[i].className += " youlose";
 			}
 		}
@@ -44,7 +49,14 @@ function resetGame(){
 	win = false;
 	outOfBounds = false;
 	statusState.innerText = 'Move your mouse over the "S" to begin.';	
-	for (var i = 0; i < boundaries.length-1; i++) {
+	for (var i = 0; i < boundaries.length - 1; i++) {
 		boundaries[i].className = "boundary";
 	}
+}
+
+function antiCheat(){
+	if(!win){ //Checks if the user trys to cheat and activates the fail state if triggered
+		overBounds();		
+		}
+
 }
