f6cef0fb5084c10745b937671d619daad1995361
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-10-21 20:38:32 -0500

    Exercise 4: Restartable maze

--- maze.js
+++ maze.js
@@ -1,8 +1,8 @@
-//#3
+//#4
 var loser = false;  // whether the user has hit a wall
 
 window.onload = function() {
-   document.getElementById("start").onclick = start;
+    document.getElementById("start").onclick = resetGame;
 	document.getElementById("end").onmouseover = end;
     var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
@@ -14,15 +14,15 @@ function overBoundary() {
     loser = true;
     var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].className += " youlose";
+       boundaries[i].className += " youlose";
     }
 }
 
-function start() {
+function resetGame() {
     loser = false;
     var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].removeClassName("youlose");
+		boundaries[i].className = "boundary";
     }
 }
 
