46c61c5c61bf03c9d748a85c44fd193e8ecca517
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-10-21 20:40:49 -0500

    Exercise 5: On-Page status updates

--- maze.js
+++ maze.js
@@ -1,35 +1,50 @@
-//#4
-var loser = false;  // whether the user has hit a wall
+//#5
+var boundaries;
+var end;
+var start;
+var statusState;
+var win = false;
+var outOfBounds = false;
 
 window.onload = function() {
-    document.getElementById("start").onclick = resetGame;
-	document.getElementById("end").onmouseover = end;
-    var boundaries = document.querySelectorAll("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].onmouseover = overBoundary;
-    }
-};
+    boundaries = document.querySelectorAll(".boundary");
+    end = document.getElementById("end");
+    start = document.getElementById("start");
+    statusState = document.getElementById("status");
 
-function overBoundary() {
-    loser = true;
-    var boundaries = document.querySelectorAll("div#maze div.boundary");
+    start.onclick = resetGame;
+    end.onmouseover = endGame;
     for (var i = 0; i < boundaries.length; i++) {
-       boundaries[i].className += " youlose";
-    }
+		boundaries[i].onmouseover = overBounds;
+	}
 }
 
-function resetGame() {
-    loser = false;
-    var boundaries = document.querySelectorAll("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
+function overBounds(){
+	if (!win){ // Used to prevent this code block from running if you have already won
+		if(!outOfBounds) // Checks if the youlose class has already been applied
+		{
+			outOfBounds = true;
+			statusState.innerText = 'You Lose! Click the "S" to reset and play again.';
+			for (var i = 0; i < boundaries.length-1; i++) {
+			boundaries[i].className += " youlose";
+			}
+		}
+	}
+}
+
+function endGame(){
+	if(!outOfBounds){ //Checks if the user hit any boundaries and lost the game
+		win = true;
+		statusState.innerText = 'You Win! Click the "S" to reset and play again.';
+		}
+
+}
+
+function resetGame(){
+	win = false;
+	outOfBounds = false;
+	statusState.innerText = 'Move your mouse over the "S" to begin.';	
+	for (var i = 0; i < boundaries.length-1; i++) {
 		boundaries[i].className = "boundary";
-    }
-}
-
-function end() {
-    if(loser) {
-        alert("Sorry, you lost. :[");
-    } else {
-        alert("You win! :]");
-    }
+	}
 }
