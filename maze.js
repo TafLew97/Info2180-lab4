db96bdcf6a079d1f12195665ec814215e3a99134
TafLew97 <32627190+TafLew97@users.noreply.github.com>
2017-10-21 20:36:13 -0500

    Exercise3: Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,26 +1,35 @@
-//#2
-var boundaries;
+//#3
+var loser = false;  // whether the user has hit a wall
+
 window.onload = function() {
-    //var boundary1 = document.getElementById("boundary1");
-    boundaries = document.querySelectorAll(".boundary");
+   document.getElementById("start").onclick = start;
+	document.getElementById("end").onmouseover = end;
+    var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
-		boundaries[i].onmouseover = overBounds;
-	}
+        boundaries[i].onmouseover = overBoundary;
+    }
+};
+
+function overBoundary() {
+    loser = true;
+    var boundaries = document.querySelectorAll("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].className += " youlose";
+    }
 }
 
+function start() {
+    loser = false;
+    var boundaries = document.querySelectorAll("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].removeClassName("youlose");
+    }
+}
 
-	function overBounds(){
-		if(!hasClass(boundaries[0], "youlose")) // Checks if the youlose class has been applied to at least one of the divs already
-		{
-			for (var i = 0; i < boundaries.length-1; i++) {
-			boundaries[i].className += " youlose";
-			}
-		}
-	}
-
-
-//Used to check if a div element already has the youlose colour applied to it to prevent
-//the onmouseover event from adding multiple instances of the class the the element
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+function end() {
+    if(loser) {
+        alert("Sorry, you lost. :[");
+    } else {
+        alert("You win! :]");
+    }
 }
